d37722ff2fc62a15915a2460a97da615c35578b6
felix t <41817193+o2bomb@users.noreply.github.com>
2022-12-24 16:11:35 +0800

    Button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,6 +5,9 @@ const Button = styled.button`
     border-radius: 0.5rem;
     background-color: #3b82f6;
     white-space: nowrap;
+    text-transform: uppercase;
+    font-weight: 600;
+    letter-spacing: 1px;
 `;
 
 export default Button;
